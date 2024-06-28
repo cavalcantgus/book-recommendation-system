9a288e7272382ca4c5053c06e73cd8fa26e0edfc
educodehub <educodehub@gmail.com>
2024-06-28 14:28:15 -0300

    Creating JPA Repositories

---
+++ src/main/java/com/cavalcantgus/book_recommendation_system/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package com.cavalcantgus.book_recommendation_system.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cavalcantgus.book_recommendation_system.entities.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long>{
+
+}
