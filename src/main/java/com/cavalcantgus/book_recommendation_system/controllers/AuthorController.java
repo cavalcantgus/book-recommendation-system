ff848671eb526e6db564ec344310aadbbd679948
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2024-07-06 19:46:46 -0300

    Resolves exception handling in AuthorController

--- src/main/java/com/cavalcantgus/book_recommendation_system/controllers/AuthorController.java
+++ src/main/java/com/cavalcantgus/book_recommendation_system/controllers/AuthorController.java
@@ -3,7 +3,10 @@ package com.cavalcantgus.book_recommendation_system.controllers;
 import java.net.URI;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cavalcantgus.book_recommendation_system.entities.Author;
+import com.cavalcantgus.book_recommendation_system.exceptions.ResourceNotFoundException;
 import com.cavalcantgus.book_recommendation_system.services.AuthorService;
 import com.cavalcantgus.book_recommendation_system.util.PatcherUpdater;
 
 @RestController
 @RequestMapping("/authors")
 public class AuthorController {
+
+	private static final Logger logger = LoggerFactory.getLogger(AuthorController.class);
 
 	@Autowired
 	private AuthorService service;
@@ -43,8 +49,8 @@ public class AuthorController {
 	}
 
 	@PostMapping
-	public ResponseEntity<Author> insert(@RequestBody Author obj) {
-		Author author = service.insert(obj);
+	public ResponseEntity<Author> insert(@RequestBody Author author) {
+		service.insert(author);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(author.getId()).toUri();
 		return ResponseEntity.created(uri).body(author);
 	}
@@ -57,16 +63,20 @@ public class AuthorController {
 
 	@PatchMapping("/{id}")
 	public ResponseEntity<Author> patch(@PathVariable Long id, @RequestBody Author author) {
-		Author obj = service.findById(id);
 
 		try {
+			Author obj = service.findById(id);
 			patcher.applyPartialUpdate(obj, author);
 			service.update(id, obj);
+			return ResponseEntity.ok().body(obj);
+		} catch (ResourceNotFoundException e) {
+			logger.error("Author with ID " + id + " not found", e);
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		} catch (Exception e) {
-			e.getStackTrace();
+			logger.error("Error while processing patch request for author with ID " + id, e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
 
-		return ResponseEntity.ok().body(obj);
 	}
 
 	@DeleteMapping("/{id}")
